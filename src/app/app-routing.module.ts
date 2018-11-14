4e7efe51c952b688cfe9bd2d2a73e355a85bfe91
Arman <dvo-rama-1994@mail.ru>
2018-11-14 19:50:10 +0400

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
+import { CreateMessageComponent } from './create-message/create-message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'home',
+		component: HomeComponent,
+	},
+	{
+		path: 'messages',
+		component: MessagesComponent,
+	},
+	{
+		path: 'create-message',
+		component: CreateMessageComponent,
+	},
+	{
+		path: '',
+		component: HomeComponent,
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { CreateMessageComponent } from './create-message/create-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    MessagesComponent,
+    CreateMessageComponent
   ],
   imports: [
     BrowserModule,
